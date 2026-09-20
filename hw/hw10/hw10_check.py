067a292685ab9994ff3186ad159fba2c11de4385
mrericreed <reederic@foothill.edu>
2026-09-19 17:58:21 -0700

    2026 to match Berkeley Data 8

--- hw/hw10/hw10_check.py
+++ hw/hw10/hw10_check.py
@@ -1,62 +1,83 @@
 from datascience import *
 import numpy as np
 
-def correctness_check_1_1(null_hypothesis, alt_hypothesis):
+def correctness_check_1_1_1(distance_example):
     try:
-        return (null_hypothesis == 4) and (alt_hypothesis == 5)
+        return np.isclose(distance_example, 5.196152422706632)
+    except Exception:
+        return False
+    
+def correctness_check_1_1_2(tradeoff):
+    try:
+        correct = [1,2,4]
+        incorrect = [3]
+        return all(x in tradeoff for x in correct) and all(x not in tradeoff for x in incorrect)
+    except Exception:
+        return False
+    
+def correctness_check_1_2(train,test,coordinates):
+    try:
+        return (train.num_rows + test.num_rows) == coordinates.num_rows
+    except Exception:
+        return False
+    
+def correctness_check_1_3(features):
+    try:
+        return sorted(features) == ['latitude', 'longitude']
+    except Exception:
+        return False
+    
+def correctness_check_1_4(classify,coordinates):
+    try:
+        sorted_coordinates = coordinates.sort("school")
+        return classify(sorted_coordinates.row(85), 3, sorted_coordinates.take(np.arange(50, 100))) == 'Stanford'
     except Exception:
         return False
 
-def correctness_check_1_2(correlation,birds):
+def correctness_check_1_5(accuracy):
     try:
-        return np.isclose(round(correlation(birds, "Egg Weight", "Bird Weight"), 3), 0.847)
+        return 0.90 <= accuracy <= 1
     except Exception:
         return False
     
-def correctness_check_1_3(fit_line, birds):
+def correctness_check_1_6(k):
     try:
-        return np.allclose(np.round(fit_line(birds, "Egg Weight", "Bird Weight"), 5), np.array([ 0.71852, -0.05827]))
+        return k == 47
     except Exception:
         return False
     
-def correctness_check_1_6(ci_conclusion):
+def correctness_check_1_7_1(train_test_split):
     try:
-        correct = [1,3,6]
-        incorrect = [2,4,5]
-        return all(x in ci_conclusion for x in correct) and all(x not in ci_conclusion for x in incorrect)
+        correct = [1,4]
+        incorrect = [2,3]
+        return all(x in train_test_split for x in correct) and all(x not in train_test_split for x in incorrect)
     except Exception:
         return False
     
-def correctness_check_1_7(confidence_interval_uses):
+def correctness_check_1_7_2(test_set_reasoning):
     try:
-        correct = [1,3]
-        incorrect = [2]
-        return all(x in confidence_interval_uses for x in correct) and all(x not in confidence_interval_uses for x in incorrect)
+        correct = [2,3]
+        incorrect = [1,4]
+        return all(x in test_set_reasoning for x in correct) and all(x not in test_set_reasoning for x in incorrect)
     except Exception:
         return False
     
-def correctness_check_2_1(egg_weight_eight):
+def correctness_check_1_8(k_reasoning):
     try:
-        return np.isclose(round(egg_weight_eight, 5), 5.68985)
-    except Exception:
-        return False
-
-def correctness_check_2_2(experts_egg):
-    try:
-        return np.isclose(round(experts_egg, 5), 6.40837)
-    except Exception:
-        return False
-
-def correctness_check_2_3(compute_resampled_line, birds):
-    try:
-        np.random.seed(0)
-        return np.allclose(compute_resampled_line(birds, "Egg Weight", "Bird Weight"), np.array([0.64163345, 0.53766856]))
+        correct = [2,4]
+        incorrect = [1,3]
+        return all(x in k_reasoning for x in correct) and all(x not in k_reasoning for x in incorrect)
     except Exception:
         return False
     
-def correctness_check_2_6(plover_statements):
+def correctness_check_1_9_1(prob_furd):
     try:
-        return plover_statements.item(0) == 3
+        return np.isclose(prob_furd, 0.4275092936802974)
     except Exception:
         return False
     
+def correctness_check_1_9_2(prob_test):
+    try:
+        return np.isclose(prob_test, 0.5)
+    except Exception:
+        return False
